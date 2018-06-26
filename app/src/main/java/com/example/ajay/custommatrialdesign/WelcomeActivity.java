c69b20bdfa1d5367ef9a4349a38f71e0c95f7fff
ajay <ajayv@selectsourceintl.com>
2018-06-26 23:58:04 +0530

    First Commit

---
+++ app/src/main/java/com/example/ajay/custommatrialdesign/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.ajay.custommatrialdesign;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
